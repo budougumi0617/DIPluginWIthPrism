e2477e9a862f89cbd02f3315695207991c6adca6
budougumi0617 <budougumi0617@gmail.com>
2017-02-16 21:38:07 +0900

    W.I.P. Study GUI programming.

---
+++ ToastNotification/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace ToastNotification
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            var mw = new MainWindow();
+            var vm = mw.DataContext as MainWindowViewModel;
+            mw.Top = vm.TopPostion;
+            mw.Left = vm.LeftPostion;
+            mw.Width = vm.Width;
+            mw.Height = vm.Heignt;
+            mw.Show();
+        }
+
+    }
+}
---
+++ ToastNotification/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Mvvm;
+
+namespace ToastNotification
+{
+    class MainWindowViewModel : BindableBase
+    {
+
+        int width = 200;
+        public int Width { get { return width; } }
+
+        int height = 300;
+        public int Heignt { get { return height; } }
+
+        public int LeftPostion
+        {
+            get
+            {
+                return (int)System.Windows.SystemParameters.PrimaryScreenWidth - Width;
+            }
+        }
+        public int TopPostion
+        {
+            get
+            {
+                return (int)System.Windows.SystemParameters.PrimaryScreenHeight - Heignt;
+            }
+        }
+    }
+}
